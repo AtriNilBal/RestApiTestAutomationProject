8bc5050af0857d788601c76d747e20cbd62766ed
unknown <atrinil@gmail.com>
2024-08-13 14:11:35 +0530

    add test method with get call

--- src/test/java/org/atrinil/api/tests/rest/ReqResTests.java
+++ src/test/java/org/atrinil/api/tests/rest/ReqResTests.java
@@ -1,14 +1,27 @@
 package org.atrinil.api.tests.rest;
 
+import io.restassured.RestAssured;
 import org.atrinil.api.config.ApiConfigFactory;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.equalTo;
 
 public class ReqResTests {
 
     private final String BASE_URL = ApiConfigFactory.getConfig().baseUrl();
+    private final String GET_SINGLE_USER_ENDPOINT = ApiConfigFactory.getConfig().getSingleUserEndpoint();
 
     @Test
     public void getSingleUserTest() {
-        System.out.println(BASE_URL);
+        RestAssured.given()
+                .baseUri(BASE_URL)
+                .log()
+                .all()
+                .when()
+                .get(GET_SINGLE_USER_ENDPOINT)
+                .then()
+                .statusCode(200)
+                .body("data.first_name", equalTo("Janet"));
     }
+    
 }
