109428a9ae91fead7ea69cab94f133aa0bd508e2
unknown <atrinil@gmail.com>
2024-08-14 17:53:26 +0530

    create payload with template json and pojo

--- src/test/java/org/atrinil/api/tests/api/testdata/UserTestData.java
+++ src/test/java/org/atrinil/api/tests/api/testdata/UserTestData.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.atrinil.api.reqresservice.pojo.UserDetails;
 
 import java.io.File;
-
-import lombok.SneakyThrows;
+import java.io.IOException;
 
 public final class UserTestData {
 
     private UserTestData() {}
 
-    @SneakyThrows
-    public static UserDetails getUserDetails() {
+    public static UserDetails getUserDetails() throws IOException {
         return new ObjectMapper()
                 .readValue(new File(System.getProperty("user.dir") + "/src/test/resources/payloadtemplates/create-user.json"), UserDetails.class)
                 .setName("atri");
