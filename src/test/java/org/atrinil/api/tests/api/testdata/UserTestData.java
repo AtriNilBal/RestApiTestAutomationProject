64e3b964001d54c6752d2075bb311708fb95932d
unknown <atrinil@gmail.com>
2024-08-14 17:55:56 +0530

    Revert "create payload with template json and pojo"
    This reverts commit 109428a9ae91fead7ea69cab94f133aa0bd508e2.

--- src/test/java/org/atrinil/api/tests/api/testdata/UserTestData.java
+++ src/test/java/org/atrinil/api/tests/api/testdata/UserTestData.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.atrinil.api.reqresservice.pojo.UserDetails;
 
 import java.io.File;
-import java.io.IOException;
+
+import lombok.SneakyThrows;
 
 public final class UserTestData {
 
     private UserTestData() {}
 
-    public static UserDetails getUserDetails() throws IOException {
+    @SneakyThrows
+    public static UserDetails getUserDetails() {
         return new ObjectMapper()
                 .readValue(new File(System.getProperty("user.dir") + "/src/test/resources/payloadtemplates/create-user.json"), UserDetails.class)
                 .setName("atri");
